55e9c5110049d93dd9cbbc885eb8927dc19dbfcb
ejabu <muhammadfahreza@gmail.com>
2019-02-21 15:08:00 +0700

    Alternative keluarin Doc di loop

--- faktur_pajak_print/models/faktur_pajak.py
+++ faktur_pajak_print/models/faktur_pajak.py
@@ -1,19 +1,40 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
+import random
 
 class FakturPajak(models.Model):
     _inherit = 'faktur.pajak'
 
     name = fields.Char('Name')
 
-    def get_invoices(self):
+    def compute_invoices(self):
         for doc in self:
             # search_query = []
             search_query = [('state', '=', 'open')]
             invoice_docs = self.env['account.invoice'].search(search_query, limit=5)
             doc.view_invoice_ids = invoice_docs
 
-    view_invoice_ids = fields.One2many('account.invoice', 'pajak_id', 'View Invoices', compute=get_invoices)
+    view_invoice_ids = fields.One2many('account.invoice', 'pajak_id', 'View Invoices', compute=compute_invoices)
+
+    def get_array(self):
+        temp_array = []
+        search_query = [('state', '=', 'open')]
+        random_offset = random.randint(1, 6)
+        invoice_docs = self.env['account.invoice'].search(search_query, limit=5, offset=random_offset)
+        for invoice_doc in invoice_docs:
+            value = {
+                'name': invoice_doc.number,
+                'amount': invoice_doc.amount_total,
+            }
+            temp_array.append(value)
+        return temp_array
+
+    def get_invoices(self):
+        temp_array = []
+        search_query = [('state', '=', 'open')]
+        random_offset = random.randint(1, 6)
+        invoice_docs = self.env['account.invoice'].search(search_query, limit=5, offset=random_offset)
+        return invoice_docs
 
 
 class AccountInvoice(models.Model):
